54bd6e8ce4e44c4a12511f76f1b91dd691c3ef0e
Ethan Turkeltaub <ethan@turkeltaub.dev>
2022-04-13 18:58:33 -0400

    Update flake

--- flake.nix
+++ flake.nix
@@ -12,13 +12,27 @@
     flake-utils.lib.eachDefaultSystem (system:
       let pkgs = nixpkgs.legacyPackages.${system};
       in rec {
-        apps = {
-          build = flake-utils.lib.mkApp {
-            drv = pkgs.writeShellScriptBin "build" ''
-              ${pkgs.zola}/bin/zola build
+        packages = {
+          build = pkgs.stdenv.mkDerivation rec {
+            name = "ethan.haus-${version}";
+            version = "0.1.0";
+            src = self;
+
+            buildInputs = with pkgs; [ zola ];
+
+            checkPhase = ''
+              zola check
+            '';
+
+            installPhase = ''
+              zola build -o $out
             '';
           };
+        };
 
+        defaultPackage = packages.build;
+
+        apps = {
           server = flake-utils.lib.mkApp {
             drv = pkgs.writeShellScriptBin "serve" ''
               ${pkgs.zola}/bin/zola serve
@@ -26,7 +40,7 @@
           };
         };
 
-        defaultApp = apps.build;
+        defaultApp = apps.server;
 
         devShell = pkgs.mkShell { buildInputs = with pkgs; [ zola ]; };
 
