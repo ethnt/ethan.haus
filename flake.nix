e2daee18d3b1ba9aa6e1b3743193ef1f923fa9b3
Ethan Turkeltaub <ethan@turkeltaub.dev>
2025-02-01 16:05:47 -0500

    Add Prettier, ESLint, CI

--- flake.nix
+++ flake.nix
@@ -4,20 +4,27 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
 
-    flake-utils.url = "github:numtide/flake-utils";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+
+    devenv.url = "github:cachix/devenv";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs {
+  outputs = inputs@{ self, flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "aarch64-darwin" ];
+
+      imports = with inputs; [ devenv.flakeModule ];
+
+      perSystem = { config, system, pkgs, ... }: {
+        _module.args.pkgs = import inputs.nixpkgs {
           inherit system;
 
           config.allowUnfree = true;
         };
-      in {
-        devShell = pkgs.mkShell {
-          buildInputs = with pkgs; [ nodejs_22 nodePackages_latest.pnpm ];
+
+        devenv.shells.default = {
+          packages = with pkgs; [ nodejs_22 nodePackages_latest.pnpm ];
         };
-      });
+      };
+    };
 }
