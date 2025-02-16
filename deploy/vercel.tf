626eae337cb87b5eef810fc7bc32fd8dde01987f
Ethan Turkeltaub <ethan@turkeltaub.dev>
2025-02-16 12:16:44 -0500

    Migrate to Vercel (#3)
    * Migrate to Vercel
    
    * Format Terraform code
    
    * Update Vercel provider
    
    * Format Terraform code

---
+++ deploy/vercel.tf
@@ -0,0 +1,16 @@
+resource "vercel_project" "ethan_haus" {
+  name      = "ethan-haus"
+  framework = "astro"
+
+  git_repository = {
+    type = "github"
+    repo = "ethnt/ethan.haus"
+  }
+
+  git_comments = {
+    on_commit       = false
+    on_pull_request = false
+  }
+
+  preview_comments = false
+}
