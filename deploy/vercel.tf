62c4c4a4ad231a3b1ca9e147e2c183ac1153541a
Ethan Turkeltaub <ethan@turkeltaub.dev>
2025-02-16 12:16:00 -0500

    Update Vercel provider

--- deploy/vercel.tf
+++ deploy/vercel.tf
@@ -6,4 +6,11 @@ resource "vercel_project" "ethan_haus" {
     type = "github"
     repo = "ethnt/ethan.haus"
   }
+
+  git_comments = {
+    on_commit = false
+    on_pull_request = false
+  }
+
+  preview_comments = false
 }
